3000916bd2849ccafea5ce0228c077261630a8f0
TeresaLiang123 <teresaliangmain@gmail.com>
2023-12-07 16:41:34 -0500

    having /user errors. server keeps crashing

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-import express from "express";
+import express from 'express'
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import cors from "cors";
 
-const express = require("express");
+mongoose.connect("mongodb://127.0.0.1:27017/MyMovieList");
+
 const app = express();
+app.use(cors());
+app.use(express.json())
+UserRoutes(app);
+
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
+
 app.listen(4000);
