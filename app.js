20fe8ce2c638ed4a6bef4cbc8b488339e2017753
caocynthia <cynthiacao.work@gmail.com>
2023-12-07 20:30:38 -0500

    fix port problem

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-import express from 'express'
+import express from "express";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
+import session from "express-session";
 import cors from "cors";
 
 mongoose.connect("mongodb://127.0.0.1:27017/MyMovieList");
 
 const app = express();
-app.use(cors());
-app.use(express.json())
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
 UserRoutes(app);
 
 app.get("/hello", (req, res) => {
