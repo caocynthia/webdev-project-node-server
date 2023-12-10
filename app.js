4c390f924e0473f918c46929cab0816f51d9188f
caocynthia <cynthiacao.work@gmail.com>
2023-12-10 18:58:53 -0500

    small change

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import ReviewsRoutes from "./reviews/routes.js";
 import "dotenv/config";
 
 const app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/MyMovieList";
