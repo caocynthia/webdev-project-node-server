f9c9bd074254b1a9d169263f153a79a2a285e8f7
caocynthia <cynthiacao.work@gmail.com>
2023-12-10 22:11:14 -0500

    fix render again

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ import "dotenv/config";
 
 const app = express();
 
-// app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/MyMovieList";
--- users/schema.js
+++ users/schema.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
       enum: ["MODERATOR", "USER"],
       default: "USER",
     },
-    likedMovies: [],
+    likedMovies: [String],
   },
   { collection: "users" }
 );
