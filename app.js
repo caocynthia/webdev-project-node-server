1fbe9c343137a2bdc7000999e02fa3e874406c30
caocynthia <cynthiacao.work@gmail.com>
2023-12-10 22:08:37 -0500

    fix render problem

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ import ReviewsRoutes from "./reviews/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/MyMovieList");
 
 const app = express();
+
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/MyMovieList";
+mongoose.connect(CONNECTION_STRING);
 app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 const sessionOptions = {
